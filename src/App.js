68629e15c6efafc0100da489b57ceda5c81493a8
Odil-jon <odiljonfullstack@gmail.com>
2021-02-05 21:43:34 +0500

    lorem

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <h1 style={{color: "green"}}> Salom </h1>
       <h1 style={{color: "red"}}>Hello Dunoy</h1>
       <h1 style={{color: "green"}}> Hello </h1> 
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ab eos, ipsa cum magnam odio aliquid, repudiandae provident eaque distinctio incidunt dolor et tenetur ullam eum animi! A, praesentium velit.
+      </h1>
     </div>
 
 
